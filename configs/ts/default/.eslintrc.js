b9bdc5ab072c38397cf5db5abff920ffe683e372
stee <71899777+steedev@users.noreply.github.com>
2021-06-13 20:50:46 +0100

    Update .eslintrc.js

--- configs/ts/default/.eslintrc.js
+++ configs/ts/default/.eslintrc.js
@@ -4,7 +4,13 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  extends: [
+    'airbnb-base',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:eslint-comments/recommended',
+    'plugin:promise/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
