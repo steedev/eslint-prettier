e1c4819271155ff6cedd3e2afc534ce1923bcddd
stee <71899777+steedev@users.noreply.github.com>
2021-06-13 14:45:19 +0100

    Update default.js

--- configs/default.js
+++ configs/default.js
@@ -3,13 +3,16 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["airbnb-base", "plugin:prettier/recommended"],
-  plugins: ["prettier"],
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
   rules: {
-    "prettier/prettier": "error",
+    'prettier/prettier': [
+      'error',
+      { semi: false, trailingComma: 'all', singleQuote: true },
+    ],
   },
-};
+}
