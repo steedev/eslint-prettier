6ab61c92e835d71c38b52f96d48037dc6b02ed91
stee <71899777+steedev@users.noreply.github.com>
2021-06-14 19:02:21 +0100

    Update .eslintrc.js

--- configs/ts/default/.eslintrc.js
+++ configs/ts/default/.eslintrc.js
@@ -1,1 +1,56 @@
-
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: './tsconfig.json',
+    createDefaultProgram: true,
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'no-console': 0,
+    'no-shadow': 'off',
+    'no-prototype-builtins': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    'prettier/prettier': [
+      'error',
+      { semi: false, trailingComma: 'all', singleQuote: true },
+    ],
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      {
+        functions: false,
+        classes: true,
+        variables: true,
+        typedefs: true,
+      },
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+}
